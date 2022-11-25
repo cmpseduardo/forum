24febe69e8a24ee67c84406a9b9e2b59c8282bc2
Eduardo Campos <98469246+cmpseduardo@users.noreply.github.com>
2022-11-25 11:21:26 -0300

    Add files via upload

--- docs/script.sql
+++ docs/script.sql
@@ -24,20 +24,24 @@ CREATE TABLE postagens(
     conteudo VARCHAR(1000) NOT NULL,
     imagem LONGBLOB NOT NULL,
     id_categoria INTEGER NOT NULL,
-    nome_categoria VARCHAR NOT NULL,
+    nome_categoria VARCHAR(30) NOT NULL,
     curtidas INTEGER NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
 
 CREATE TABLE respostas(
-    id_postagem INTEGER NOT NULL,
     id_resposta INTEGER NOT NULL PRIMARY KEY,
+    id_postagem INTEGER NOT NULL,
     comentario VARCHAR(1000),
+    FOREIGN KEY (id_postagem) REFERENCES postagens(id_postagem)
 );
 
 CREATE TABLE subcomentarios(
+    id_subcomentario INTEGER NOT NULL PRIMARY KEY,
     id_postagem INTEGER NOT NULL,
     id_resposta INTEGER NOT NULL,
-    subcomentario VARCHAR(1000)
+    subcomentario VARCHAR(1000) NOT NULL,
+    FOREIGN KEY (id_resposta) REFERENCES respostas(id_resposta)
 );
 
 DESCRIBE usuarios;
@@ -46,3 +50,14 @@ DESCRIBE postagens;
 DESCRIBE respostas;
 DESCRIBE subcomentarios;
 SHOW TABLES;
+
+CREATE VIEW vw_respostas AS 
+SELECT * FROM respostas r
+INNER JOIN postagens p ON p.id_postagem = r.id_postagem
+INNER JOIN subcomentarios s ON r.id_resposta = s.id_resposta;
+
+CREATE VIEW vw_respostas AS 
+SELECT p.id_postagem AS id_post, p.titulo, p.conteudo, r.id_resposta AS id_resp, r.comentario, s.id_subcomentario, s.subcomentario
+FROM respostas r
+INNER JOIN postagens p ON p.id_postagem = r.id_postagem
+INNER JOIN subcomentarios s on s.id_resposta = r.id_resposta;
